ba99231c5119ffe57c9b67dd580f65bd0beb1bd2
João Victor Santana <jvdev22@hotmail.com>
2024-04-24 19:44:26 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,59 +1,67 @@
 import os
 import time
 
-saldo = 500
+saldo = 0
+limite_500 = 0.0
 resposta = 'sim'
 depositos_lista = []
 saque_lista = []
 
 print('Bem-vindo(a) ao Banco Star')
 
-while resposta == 'sim': 
+while resposta.lower().startswith('s'):
     print('''
       
+      [0] Sair
       [1] Depósito
       [2] Saque
       [3] Extrato''')
 
+#VerificandoRespostaDoUsuario
     try:
         escolha = int(input('Qual operação você deseja realizar?: '))
     except ValueError:
         print("Por favor, digite um número válido.")
 
-    time.sleep (2)
+    time.sleep(2)
     os.system('cls')
 
+ #OperaçãoDeDepósito:
     if escolha == 1:
         deposito = float(input('Qual valor deseja depositar?: '))
         depositos_lista.append(deposito)
-        time.sleep (2)
+        time.sleep(2)
         os.system('cls')
         if deposito < 1:
             print('Valor informado não pode ser adicionado, tente novamente...')
         elif deposito > 0:
             saldo += deposito
             print("Esse é o seu saldo atual: {}".format(saldo))
-            resposta = str(input("deseja continuar com essa operação?: "))
+            resposta = str(input("Deseja continuar com essa operação?: "))
+    
+    #OperaçãoDeSaque:
     elif escolha == 2:
         saque = float(input("Qual valor deseja sacar?: "))
         if saque < saldo and saque <= 500:
-            saque_lista.append(saque)
-            time.sleep(3)
-            os.system('cls')
+            if saque == 500:
+                if limite_500 < 3:
+                    limite_500 += 1 
+                else:
+                    print("Limite diário de 3 saques de R$500 excedido...")
+                    continue
 
+            saque_lista.append(saque)        
             saldo -= saque
             print("Esse é o seu saldo atual: {}".format(saldo))
             resposta = str(input("Deseja continuar com a operação?: "))
 
         elif saque > saldo:
-
             print("Você não tem saldo suficiente para realizar essa operação...")
-        
+                
         elif saque > 500:
+            print("Você ultrapassou o limite de R$500...")
 
-            print("Você ultrapassou o limite diário de R$500...")
-           
-       
+#VerificandoExtrato
     elif escolha == 3:
         print("Operações realizadas em sua conta nos ultimos dias: ")
         print("--------------------------------------------------")
@@ -61,17 +69,15 @@ while resposta == 'sim':
         print("""Lista de depósitos realizados: 
                 {} """.format(depositos_lista))
         
-       
+        print("-----------------------------------------------------")
+        
         print('''Lista de saques realizados: 
                 {}'''.format(saque_lista))
+        
+        resposta = str(input("Deseja continuar com a operação?: "))
     else:
         break
 
 
 print("Operação finalizada...")
-
-           
-            
-
-
 
